5c9309ab7b1f197c3e7ece8abb07eb1c50fbf637
VikramSingh2001 <120383040+VikramSingh2001@users.noreply.github.com>
2024-08-12 14:27:57 +0530

    Delete myworld directory

--- myworld/src/b.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a=10;
-    // int b=a++;
-    // int c=++a;
-    printf("%d %d %d",(a),(a++),(++a));
-    // printf("%d %d %d",(a),(b),(c));
-    // printf("%d %d %d",a,++a,a++);
-}
